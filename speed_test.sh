23ed23a1dcdfa7a0e9d03046cd4157b24ef75282
abc <a@b.c>
2018-05-14 20:18:50 -0400

    minor updates

--- speed_test.sh
+++ speed_test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-set -x
+set +x
 
 : ${1? echo "Usage: $0 <file>" }
 
 _f=$1
 
-time cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8192 | head -n 8192 > $_f
-time curl -X POST -F file=@$_f localhost:8000
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8192 | head -n 8192 > $_f
+
+echo "Testing curl..."
+time curl -X POST -F file=@$_f -s -o /dev/null localhost:8888
+
+echo "Testing wget..."
+time wget -q --post-file $_f http://localhost:8888 -O /dev/null 
