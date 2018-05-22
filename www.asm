6520cd428946d41f7a7336990ebeac9dacabcc7e
Igor Nesvit <inesvit@gmail.com>
2018-05-22 17:34:59 -0400

    changes

--- www.asm
+++ www.asm
@@ -2,17 +2,24 @@
 
 section .data
 
-msg db      'w','w' ; w hex - 77, makes it easier to find in memory
+msg db      	'w','w','w' ; www hex - 777777, makes it easier to find in memory
+b1 db		'+','*','+','.'; 2B2A2B2E
+b2 db		'.','+','*','+' ; 7B4B37
+
 len equ     $ - msg
 
 section .text 
 global  _start              
 
 _pre:
-	db 0x90,0x90,0x90,0xc3 ; direct raw instructions , run disassemble /r (nop nop nop ret)
+	db 0x90,0x90,0x90 ; direct raw instructions , run disassemble /r (nop nop nop ret)
+	lea rbx, [b2]
+        mov rdx, [b2]
+	db 0xc3
 _start:
 	call _pre
 	call _post
+	nop
 	push rbp
 	mov rbp, rsp
 	sub rsp, 0x40
