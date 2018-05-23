9822d2d873eebf6a81024f031faab7c8039caaf0
Igor Nesvit <inesvit@gmail.com>
2018-05-23 16:54:10 -0400

    few small changes

--- www.asm
+++ www.asm
@@ -2,23 +2,22 @@
 
 section .data
 
-msg db      	'w','w','w' ; www hex - 777777, makes it easier to find in memory
-b1 db		'+','*','+','.'; 2B2A2B2E
-b2 db		'.','+','*','+' ; 7B4B37
-
-len equ     $ - msg
+s1 db		"leet"
+s2 db		"coffee"
 
 section .text 
 global  _start              
 
 _pre:
-	db 0x90,0x90,0x90 ; direct raw instructions , run disassemble /r (nop nop nop ret)
-	lea rbx, [b2]
-        mov rdx, [b2]
-	db 0xc3
+	db 0x90,0x90,0x90 ; NOP direct raw instructions , run disassemble /r (nop nop nop ret)
+	lea rbx, [s1]
+        lea rdx, [s2]
+	db 0xc3		; RET
 _start:
 	call _pre
 	call _post
+	nop
+	nop
 	nop
 	push rbp
 	mov rbp, rsp
@@ -31,13 +30,13 @@ _start:
 	add rdx, rcx
 	mov [rbp-0x20], rdx
 	nop
-        lea rbx, [msg] 
+	nop
 	nop
 	mov     rax, 60	; sys_exit   
 	mov	rdi, [rbp-0x20]	; retcode is the sum of numbers 
 	syscall
 _post:
         mov     rax, 60 ; sys_exit
-        mov     rdi, 0 
+        mov     rdi, 88 
         syscall
 	ret
