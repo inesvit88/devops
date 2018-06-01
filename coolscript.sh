5c894e9cdded173049750c3cc22e8eeb9af1eecb
user <inesvit@gmail.com>
2018-05-31 23:15:00 -0400

    1

--- coolscript.sh
+++ coolscript.sh
@@ -1,1 +1,1 @@
-ps -aux | awk '{print $2}' | xargs -0 echo > pids && for i in $(cat ./pids); do sudo lsof -p $i 2> /dev/null; done | grep -i "ipv4\|ipv6"
+ps -aux | awk '{print $2}' | xargs -0 echo > pids && for i in $(cat ./pids); do lsof -p $i 2> /dev/null; done | grep -i "ipv4\|ipv6"
