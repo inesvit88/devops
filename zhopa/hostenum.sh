e0b6d3fb2513999ee21dc25715454274cd8a5bbd
me <me@someplace.io>
2018-07-22 14:27:20 -0400

    ...bug linting...

--- zhopa/hostenum.sh
+++ zhopa/hostenum.sh
@@ -7,7 +7,7 @@ IFNAME=$1
 zaspinner()
 {
   local pid=$1
-  local delay=0.1
+  local delay=0.07
   local spinstr='|/-\'
   while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
     local temp=${spinstr#?}
@@ -23,11 +23,13 @@ grep_ips_from_file(){
   sed -n 's/\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}/\nchop&\n/gp' $1 | grep chop | tr -d 'chop' | sort -u
 }
 
-300baud(){
+ctype(){
   str=$1
+  speed=$2
+  if [ -z $speed ]; then speed=".03"; fi
   for (( i=0; i<${#str}; i++ )); do
     echo -n "${str:$i:1}"
-    sleep .03
+    sleep $speed
   done
   echo 
 }
@@ -52,31 +54,17 @@ network_CIDR(){
 }
 netscan(){
   NETWORK=$( network_CIDR $1 )
-  300baud "[.] scaning the network $NETWORK for running hosts.."
-  330baud "[*] launching a stealth SYN scan against each machine that is up out of 256 IPs on the class C sized network..."
+  ctype "[.] scaning the network $NETWORK for running hosts.." .01
+  ctype "[*] launching a stealth SYN scan against each machine that is up out of 256 IPs on the class C sized network..." .01
   if [ -d ./nmap ]; then rm -rf ./nmap; fi
-  mkdir nmap
-  nmap -sS -oA nmap/init $NETWORK > /dev/null 2>&1 &
-  echo -n "[.] get to da chOppA!!! ==> "; zaspinner $!
+  mkdir ./nmap
+  sudo nmap -sS -oA nmap/init $NETWORK > /dev/null 2>&1 &
+  echo -n "[*] get to da chOppA!!! ==> "; zaspinner $!
   grep_ips_from_file nmap/*
   exit 1
-}
-
-recon(){
-  echo "[*] running recon and score against the rhost $1"
-  rr_vector=$(ssh -q -t $1 < $2 | tee -a results | grep resultvector | sed -e 's/^\w*\ *//') 
-  echo "[.] recon results vector obtained => [$rr_vector]"
-  rrv_expr=$(echo $rr_vector | tr ' ' '+')
-  score=$(python3 -c "print($rrv_expr)")
-  echo "[.] target score ==> $score"
-  if [ $score == "8" ];
-    then
-      300baud "[.] Dang....most likely no Internet connectivity" 
-  fi
 }
 
 netscan $IFNAME
 showoff
 print_scoring_matrix $CMDFILE
-300baud "[+] Starting remote code execution..."
-recon $RHOSTS $CMDFILE
+ctype "[+] Starting remote code execution..."
