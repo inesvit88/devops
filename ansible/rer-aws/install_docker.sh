c32111b67062da4231d6ea850181fb056bd89fc3
Igor <inesvit@gmail.com>
2018-12-28 01:17:04 +0000

    changes...

--- ansible/rer-aws/install_docker.sh
+++ ansible/rer-aws/install_docker.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-ansible-playbook -i ./hosts install_docker.yml --private-key "~/.ssh/dev2.pem"
+ansible-playbook -i ./hosts install_docker.yml --private-key "~/.ssh/dev2.pem" -u ec2-user
